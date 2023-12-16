99a664b6def244cb6da2233ae45ab9cece08b4ad
DucAnh2611 <117668851+DucAnh2611@users.noreply.github.com>
2023-12-17 00:11:56 +0700

    cors for all site

--- server/app.js
+++ server/app.js
@@ -19,6 +19,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/document', documentRouter);
