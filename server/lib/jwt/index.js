5c9017f350b26b21517f5e8a2e9708ecc9b6021f
DucAnh2611 <117668851+DucAnh2611@users.noreply.github.com>
2023-12-16 13:33:30 +0700

    Fix bug refresh token

--- server/lib/jwt/index.js
+++ server/lib/jwt/index.js
@@ -8,14 +8,12 @@ const verifyToken = (req, res, next) => {
     req.body.token || req.query.token || req.headers["x-access-token"] || JSON.parse(req.cookies[config.cookie]).token;
 
     if (!token) {
-      return res.status(403).send("A token is required for authentication");
+      return res.status(403).json({status: "fail", message: "A token is required for authentication"});
     }
     const decoded = jwt.verify(token, config.TOKEN_KEY);
 
     if(parseInt(new Date().getTime()/1000 - decoded.exp) > 0 ) {
-      res.cookie(config.cookie, JSON.stringify({
-        token: newToken(decoded, "72h")
-      }));
+      return res.status(403).json({status: "fail", message: "Expired Token"});
     }
     else {
       req.user = {
